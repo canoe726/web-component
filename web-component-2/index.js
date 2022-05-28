76fb7b9ec92c346486295543e9aee35e8f4a5b27
canoe726 <canoe918@gmail.com>
2022-05-29 04:25:22 +0900

    add component class

--- web-component-2/index.js
+++ web-component-2/index.js
@@ -1,38 +1,59 @@
-const app = document.querySelector('#root');
-
-let state = {
-  items: ['item1', 'item2', 'item3', 'item4']
+class Component {
+  $target;
+  $state;
+  constructor ($target) {
+    this.$target = $target;
+    this.init();
+    this.render();
+  }
+  init () {}
+  template () {}
+  render () {
+    this.$target.innerHTML = this.template();
+    this.setEvent();
+  }
+  setEvent () {}
+  setState (newState) {
+    this.$state = { ...this.$state, ...newState };
+    this.render();
+  }
 }
 
-const render = () => {
-  const { items } = state;
-  app.innerHTML = `
-    <ul>
-      ${items.map((item) => {
-        return `
-          <li>${item}</li>
-        `;
-      }).join('')}
-    </ul>
-    <button id="append">Add</button>
-  `
-  const btn = document.getElementById('append');
-  btn.addEventListener('click', () => {
-    setState({
-      items: [
-        ...items,
-        `item${items.length + 1}`,
-      ]
-    })
-  });
+class App extends Component {
+  init () {
+    this.$state = {
+      items: ['item1', 'item2', 'item3', 'item4']
+    };
+  }
+
+  template () {
+    const { items } = this.$state;
+    return `
+      <ul>
+        ${items.map((item) => {
+          return `
+            <li>${item}</li>
+          `;
+        }).join('')}
+      </ul>
+      <button id="append">Add</button>
+    `;
+  }
+
+  setEvent () {
+    const btn = this.$target.querySelector('#append');
+    btn.addEventListener('click', () => {
+      const { items } = this.$state;
+      this.setState({
+        ...this.$state,
+        items: [
+          ...items,
+          `item${items.length + 1}`,
+        ]
+      })
+    });  
+  }
 }
 
-const setState = (newState) => {
-  state = {
-    ...state,
-    ...newState,
-  };
-  render();
-}
-
-render();
+const root = document.querySelector('#root');
+new App(root);
