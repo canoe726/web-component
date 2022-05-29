307404f7961f552a208391a2ac188f4c8a04287c
canoe726 <canoe918@gmail.com>
2022-05-29 16:22:21 +0900

    detach all components

--- web-component-2/App.js
+++ web-component-2/App.js
@@ -1,7 +1,16 @@
 import Component from './cores/Component.js';
 import MyListHeader from './src/components/myListHeader.js';
+import MyListMain from './src/components/myListMain.js';
+import MyListFooter from './src/components/myListFooter.js';
 
 class App extends Component {
+  get filteredItems () {
+    const { isFilter, items } = this.$state;
+    return items.filter(({ active }) => (isFilter === 1 && active) ||
+                                        (isFilter === 2 && !active) ||
+                                        (isFilter === 0));
+  }
+
   init () {
     this.$state = {
       isFilter: 0,
@@ -28,17 +37,29 @@ class App extends Component {
   template () {
     return `
       <header data-component="item-appender"></header>
-      <main data-component="items></main>
+      <main data-component="item-main"></main>
       <footer data-component="item-filter"></footer>
     `;
   }
 
   mounted () {
-    const { addItem } = this;
+    const { addItem, filteredItems, deleteItem, toggleItem, filterItems } = this;
 
     const $myListHeader = this.$target.querySelector('[data-component="item-appender"]');
     new MyListHeader($myListHeader, {
       addItem: addItem.bind(this),
+    });
+
+    const $myListMain = this.$target.querySelector('[data-component="item-main"]');
+    new MyListMain($myListMain, {
+      filteredItems,
+      deleteItem: deleteItem.bind(this),
+      toggleItem: toggleItem.bind(this),
+    });
+
+    const $myListFooter = this.$target.querySelector('[data-component="item-filter"]');
+    new MyListFooter($myListFooter, {
+      filterItems: filterItems.bind(this),
     });
   }
 
@@ -53,6 +74,36 @@ class App extends Component {
         { seq, contents, active }
       ],
     });
+  }
+
+  deleteItem (index) {
+    const { items } = this.$state;
+    items.splice(items.findIndex(item => item.seq === index), 1);
+    this.setState({
+      ...this.$state,
+      items: [
+        ...items
+      ]
+    })
+  }
+
+  toggleItem (idx) {
+    const { items } = this.$state;
+    const index = items.findIndex(item => item.seq === idx);
+    items[index].active = !items[index].active;
+    this.setState({
+      ...this.$state,
+      items: [
+        ...items,
+      ]
+    })
+  }
+
+  filterItems (target) {
+    this.setState({
+      ...this.$state,
+      isFilter: Number(target.dataset.isFilter),
+    })
   }
 }
 
--- web-component-2/src/components/myListMain.js
+++ web-component-2/src/components/myListMain.js
@@ -1,78 +1,38 @@
 import Component from "../../cores/Component.js";
 
-class MyList extends Component {
-  get filteredItems () {
-    const { isFilter, items } = this.$state;
-    console.log(isFilter)
-    return items.filter(({ active }) => (isFilter === 1 && active) ||
-                                        (isFilter === 2 && !active) ||
-                                        (isFilter === 0));
-  }
-
+class MyListMain extends Component {
   template () {
-    const filteredItems = this.filteredItems;
+    const { filteredItems } = this.$props;
     return `
-      <header>
-        <input type="text" class="appender" placeholder="아이템 내용 입력"/>
-      </header>
-      <main>
-        <ul>
-          ${filteredItems.map(({ seq, contents, active }) => {
-            return `
-              <li data-seq="${seq}">
-                ${contents}
-                <button class="toggleBtn" style="color: ${active ? '#09F' : '#F09'}">
-                  ${active ? '활성' : '비활성'}
-                </button>
-                <button class="deleteBtn">삭제</button>
-              </li>
-            `;
-          }).join('')}
-        </ul>
-      </main>
-      <footer>
-        <button class="filterBtn" data-is-filter="0">전체 보기</button>
-        <button class="filterBtn" data-is-filter="1">활성 보기</button>
-        <button class="filterBtn" data-is-filter="2">비활성 보기</button>
-      </footer>
+      <ul>
+        ${filteredItems.map(({ seq, contents, active }) => {
+          return `
+            <li data-seq="${seq}">
+              ${contents}
+              <button class="toggleBtn" style="color: ${active ? '#09F' : '#F09'}">
+                ${active ? '활성' : '비활성'}
+              </button>
+              <button class="deleteBtn">삭제</button>
+            </li>
+          `;
+        }).join('')}
+      </ul>
     `;
   }
 
   setEvent () {
+    const { deleteItem, toggleItem } = this.$props;
 
     this.addEvent('click', '.deleteBtn', ({ target }) => {
-      const { items } = this.$state;
       const seq = Number(target.closest('[data-seq]').dataset.seq);
-      items.splice(items.findIndex(item => item.seq === seq), 1);
-      this.setState({
-        ...this.$state,
-        items: [
-          ...items
-        ]
-      })
+      deleteItem(seq);
     });
 
     this.addEvent('click', '.toggleBtn', ({ target }) => {
-      const { items } = this.$state;
       const seq = Number(target.closest('[data-seq]').dataset.seq);
-      const index = items.findIndex(item => item.seq === seq);
-      items[index].active = !items[index].active;
-
-      this.setState({
-        ...this.$state,
-        items: [
-          ...items,
-        ]
-      })
-    });
-
-    this.addEvent('click', '.filterBtn', ({ target }) => {
-      this.setState({
-        ...this.$state,
-        isFilter: Number(target.dataset.isFilter),
-      })
+      toggleItem(seq);
     });
   }
 }
 
-export default MyList;
+export default MyListMain;
