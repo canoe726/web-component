9f5eb719799782bdf924f593509201a4f22b5ca0
canoe726 <canoe918@gmail.com>
2022-05-29 23:35:49 +0900

    add createElement

--- virtual-dom/src/main.js
+++ virtual-dom/src/main.js
@@ -3,10 +3,41 @@ function h(type, props, ...children) {
   return { type, props, children: children.flat() };
 }
 
-const vm = (
+function createElement(node) {
+  if (typeof node === 'string') {
+    return document.createTextNode(node);
+  }
+  const $elem = document.createElement(node.type);
+
+  Object.entries(node.props || {})
+        .filter(([attr, value]) => value)
+        .forEach(([attr, value]) => (
+          $elem.setAttribute(attr, value)
+        ));
+
+  const children = node.children.map(createElement);
+  children.forEach(child => $elem.appendChild(child));
+  return $elem;
+}
+
+const state = [
+  { id: 1, completed: false, content: 'todo list item 1' },
+  { id: 2, completed: true, content: 'todo list item 2' },
+]
+
+const vm = createElement(
   <div id="app">
     <ul>
-      <li>
+      {state.map(({ id, completed, content }) => {
+        return (
+          <li id={id} class={completed ? 'completed' : null}>
+            <input type="checkbox" class="toggle" checked={completed}/>
+            {content}
+            <button class="remove">삭제</button>
+          </li>
+        )
+      })}
+      {/* <li>
         <input type="checkbox" class="toggle"/>
         todo list item 1
         <button class="remove">삭제</button>
@@ -15,7 +46,7 @@ const vm = (
         <input type="checkbox" class="toggle"/>
         todo list item 2
         <button class="remove">삭제</button>
-      </li>
+      </li> */}
     </ul>
     <form>
       <input type="text"/>
@@ -25,6 +56,7 @@ const vm = (
 )
 
 const $root = document.getElementById('root');
-$root.innerHTML = `
-  <pre>${JSON.stringify(vm, null, 2)}</pre>
-`
+// $root.innerHTML = `
+//   <pre>${JSON.stringify(vm, null, 2)}</pre>
+// `
+$root.appendChild(vm)
--- virtual-dom/src/virtualDom.js
+++ virtual-dom/src/virtualDom.js
@@ -21,21 +21,33 @@ function h (type, props, ...children) {
 </form>
 */}
 
+const state = [
+  { id: 1, completed: false, content: 'todo list item 1' },
+  { id: 2, completed: true, content: 'todo list item 2' },
+]
+
 const vDom =
 h('div', { id: 'app' },
-  h('ul', null, 
-    h('li', null,
-      h('input', { type: 'checkbox', class: 'toggle' },
-        'todo list item 1',
-        h('button', { class: 'remove' }, '삭제'),
-      )
-    ),
-    h('li', { class: 'completed' },
-      h('input', { type: 'checkbox', class: 'toggle' },
-        'todo list item 2',
+  h('ul', null,
+    state.map({ completed, content }, () => {
+      return h('li', completed ? { completed: true } : null,
+        h('input', { type: 'checkbox', class: 'toggle', checked: completed }),
+        content,
         h('button', { class: 'remove' }, '삭제')
       )
-    ),
+    }),
+    // h('li', null,
+    //   h('input', { type: 'checkbox', class: 'toggle' },
+    //     'todo list item 1',
+    //     h('button', { class: 'remove' }, '삭제'),
+    //   )
+    // ),
+    // h('li', { class: 'completed' },
+    //   h('input', { type: 'checkbox', class: 'toggle' },
+    //     'todo list item 2',
+    //     h('button', { class: 'remove' }, '삭제')
+    //   )
+    // ),
     h('form', null,
       h('input', { type: 'text' }),
       h('button', { type: 'submit' }, '추가'),
