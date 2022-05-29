f78a50340ec1fafb776fcc16d01cf67519006f57
canoe726 <canoe918@gmail.com>
2022-05-29 20:57:28 +0900

    use root store

--- web-component-2/App2.js
+++ web-component-2/App2.js
@@ -1,36 +1,40 @@
 import Component from './cores/Component.js'
+import { store } from './store/store.js';
+
+const Result = () => {
+  return `<p>a + b = ${store.state.a + store.state.b}</p>`;
+}
+
+const InputA = () => {
+  return `<input id="stateA" value="${store.state.a}"/>`;
+}
+
+const InputB = () => {
+  return `<input id="stateB" value="${store.state.b}"/>`;
+}
 
 class App2 extends Component {
-  initState () {
-    return this.$state = {
-      a: 10,
-      b: 20,
-    };
-  }
-
   template () {
-    const { a, b } = this.$state;
-
     return `
-      <p>a + b = ${a + b}</p>
-      <input id="stateA" value="${a}"/>
-      <input id="stateB" value="${b}"/>
+      ${Result()}
+      ${InputA()}
+      ${InputB()}
     `;
   }
 
   setEvent () {
     this.addEvent('keyup', '#stateA', ({ key, target }) => {
       if (key !== 'Enter') return false;
-      this.setState({
-        ...this.$state,
+      store.setState({
+        ...store.state,
         a: Number(target.value),
       });
     });
 
     this.addEvent('keyup', '#stateB', ({ key, target }) => {
       if (key !== 'Enter') return false;
-      this.setState({
-        ...this.$state,
+      store.setState({
+        ...store.state,
         b: Number(target.value),
       });
     });
